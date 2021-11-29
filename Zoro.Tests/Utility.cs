3f03391465db20e68663f5fd9d472d653bae0449
Jim (Dimitrios) Andrakakis <dandraka@gmail.com>
2021-11-29 18:46:03 +0100

    added sqllite db test infra

--- Zoro.Tests/Utility.cs
+++ Zoro.Tests/Utility.cs
@@ -1,19 +1,21 @@
 using System;
+using System.Data;
 using System.Data.Common;
 using System.IO;
 using System.Reflection;
+using System.Text;
+using System.Data.SQLite;
+using System.Collections.Generic;
 
 namespace Dandraka.Zoro.Tests
 {
-    internal class Utility: IDisposable
+    internal class Utility : IDisposable
     {
         public string TestInstanceDir;
         public string TestInstanceConfigfile;
         public string TestDataDir => Path.Combine(Utility.AssemblyDirectory, "data");
 
         public DbConnection TestDbConnection;
-
-        private string compactDbFile;        
 
         public Utility()
         {
@@ -26,35 +28,47 @@ namespace Dandraka.Zoro.Tests
 
         public void PrepareLocalDb()
         {
-            this.compactDbFile = Path.GetTempFileName();
-            string connectionString = $"Data Source={compactDbFile};Persist Security Info=False";
-
-            Console.WriteLine($"Created Db file {this.compactDbFile}");
-
-            this.TestDbConnection = DbProviderFactories.GetFactory("System.Data.SqlServerCe.4.0").CreateConnection();
-            this.TestDbConnection.ConnectionString = connectionString;
+            this.TestDbConnection = new SQLiteConnection("Data Source=:memory:");
             this.TestDbConnection.Open();
 
             var cmdCreateTable = this.TestDbConnection.CreateCommand();
             cmdCreateTable.CommandType = System.Data.CommandType.Text;
             cmdCreateTable.CommandText = "CREATE TABLE csvdata (ID int, Name nvarchar(100), BankAccount varchar(50))";
             cmdCreateTable.ExecuteNonQuery();
-            
-            var csvContents = File.ReadLines(Path.Combine(TestDataDir, "data1.csv"));
-            foreach(string csvLine in csvContents)
-            {
-                if (csvLine.StartsWith("ID"))
-                {
-                    continue;
-                }
-                var csvFields = csvLine.Split(';');
-                string insertSql = $"INSERT INTO csvdata (ID, Name, BankAccount) VALUES ({csvFields[0]},'{csvFields[1]}','{csvFields[2]}')";
 
-                var cmdInsert = this.TestDbConnection.CreateCommand();
-                cmdInsert.CommandType = System.Data.CommandType.Text;
-                cmdInsert.CommandText = insertSql;
-                cmdInsert.ExecuteNonQuery();                
+            var csvContents = new List<string>(File.ReadLines(Path.Combine(TestDataDir, "data1.csv")));
+            var csvFields = csvContents[0].Split(';');
+
+            string insertSql = $"INSERT INTO csvdata (ID, Name, BankAccount) VALUES ($ID, $Name, $BankAccount)";
+            var cmdInsert = this.TestDbConnection.CreateCommand();
+            cmdInsert.CommandType = System.Data.CommandType.Text;
+            cmdInsert.CommandText = insertSql;
+            foreach (string csvField in csvFields)
+            {
+                cmdInsert.Parameters.Add(new SQLiteParameter($"${csvField}"));
             }
+
+            for (int i = 1; i < csvContents.Count; i++)
+            {
+                string csvLine = csvContents[i];
+                var csvLineFields = csvLine.Split(';');
+                for (int j = 0; j < csvLineFields.Length; j++)
+                {
+                    cmdInsert.Parameters[j].Value = csvLineFields[j];
+                }
+                cmdInsert.ExecuteNonQuery();
+            }
+
+            var cmdSelect = this.TestDbConnection.CreateCommand();
+            cmdSelect.CommandType = System.Data.CommandType.Text;
+            cmdSelect.CommandText = "select * from csvdata";
+            var rd = cmdSelect.ExecuteReader();
+            var tbl = new DataTable();
+            tbl.Load(rd);
+            string tblContents = DumpDataTable(tbl);
+            Console.WriteLine("============= Initial DB data =============");
+            Console.Write(tblContents);
+            Console.WriteLine("===========================================");
         }
 
         public void PrepareTestInstanceDir()
@@ -64,7 +78,7 @@ namespace Dandraka.Zoro.Tests
                 File.Copy(filename, Path.Combine(TestInstanceDir, Path.GetFileName(filename)), true);
                 //Console.WriteLine($"Copied {filename} to {TestInstanceDir}");
             }
-            
+
             if (!File.Exists(TestInstanceConfigfile))
             {
                 throw new FileNotFoundException(TestInstanceConfigfile);
@@ -90,7 +104,7 @@ namespace Dandraka.Zoro.Tests
                 {
                     Directory.Delete(this.TestInstanceDir, true);
                 }
-                if (this.TestDbConnection != null 
+                if (this.TestDbConnection != null
                     && this.TestDbConnection.State == System.Data.ConnectionState.Open)
                 {
                     this.TestDbConnection.Close();
@@ -109,6 +123,28 @@ namespace Dandraka.Zoro.Tests
                 string codebaseLocation = Assembly.GetExecutingAssembly().Location;
                 return Path.GetDirectoryName(codebaseLocation);
             }
+        }
+
+        private static string DumpDataTable(DataTable table)
+        {
+            string data = string.Empty;
+            StringBuilder sb = new StringBuilder();
+
+            if (null != table && null != table.Rows)
+            {
+                foreach (DataRow dataRow in table.Rows)
+                {
+                    foreach (var item in dataRow.ItemArray)
+                    {
+                        sb.Append(item);
+                        sb.Append(',');
+                    }
+                    sb.AppendLine();
+                }
+
+                data = sb.ToString();
+            }
+            return data;
         }
     }
 }
